f52e3ca0c100fabe25128a61d7f6bda79ea5bdfc
Nicolay <nicolaymorenoherrera@gmail.com>
2024-02-21 18:48:32 -0500

    Starting

---
+++ src/assets/data.js
@@ -0,0 +1,22 @@
+export const data = [
+   {
+      id: 1,
+      imgUrl: "./Images/playStation.jpg",
+   },
+   {
+      id: 2,
+      imgUrl: "./Images/xbox.jpg",
+   },
+   {
+      id: 3,
+      imgUrl: "./Images/nintendo.jpg",
+   },
+   {
+      id: 4,
+      imgUrl: "./Images/nintendo2.jpg",
+   },
+   {
+      id: 5,
+      imgUrl: "./Images/playStation2.jpg",
+   },
+];
